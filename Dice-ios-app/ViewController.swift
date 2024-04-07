786b3dd14ee9fff1413d1682988abda03242211b
karthiksagarN <karthik.sagarn@gmail.com>
2024-04-07 20:14:20 +0530

    updated app

--- Dice-ios-app/ViewController.swift
+++ Dice-ios-app/ViewController.swift
@@ -10,25 +10,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImage1: UIImageView!
     @IBOutlet weak var diceImage2: UIImageView!
     
+    var leftDiceNum = 1
+    var rightDiceNum = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Who.What = Value
-        diceImage1.image = UIImage(named: "DiceSix")
+//        diceImage1.image = UIImage(named: "DiceSix")
+        
+        let diceArray_login = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImage1.image =  diceArray_login[Int.random(in: 0...5)]
+        diceImage2.image  = diceArray_login[Int.random(in: 0...5)]
+        
+        
+// syntax for image literals - diceImage1.image = #imageLiteral(resourceName: "DiceTwo")
+
+        //diceImage1.image = #imageLiteral(resourceName: "DiceTwo")
         // Who.What = Value
         // diceImage1.alpha = 0.5
         
         // Who.What = Value
-        diceImage2.image = UIImage(named: "DiceTwo")
+//        diceImage2.image = UIImage(named: "DiceTwo")
         // Who.What = Value
         // diceImage2.alpha = 0.5
         
     }
     @IBAction func rollButtonPressed(_ sender: Any) {
-        print("button tapped")
-        diceImage1.image = UIImage(named: "DiceThree")
-        diceImage2.image = UIImage(named: "DiceThree")
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImage1.image =  diceArray[Int.random(in: 0...5)]
+        diceImage2.image  = diceArray[Int.random(in: 0...5)]
+//        leftDiceNum = leftDiceNum+1
+//        rightDiceNum = rightDiceNum-1
+        
+//        print(Int.random(in: 1...6))
+        
     }
     
 }
